35449af99625df0f5d055111a932a6eec6b2739a
ahocquard <alexandre.hocquard@akeneo.com>
2019-04-28 20:27:37 +0200

    Create a product and its normalizer

---
+++ src/Domain/Model/Product.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Model;
+
+final class Product
+{
+    /** @var string */
+    private $identifier;
+
+    /** @var string[] */
+    private $categories;
+
+    /**
+     * @param string   $identifier
+     * @param string[] $categories
+     */
+    public function __construct(string $identifier, array $categories)
+    {
+        $this->identifier = $identifier;
+        $this->categories = (function(string ...$categories) {
+            return $categories;
+        })(...$categories);
+    }
+
+    public function identifier(): string
+    {
+        return $this->identifier;
+    }
+
+    public function categories(): array
+    {
+        return $this->categories;
+    }
+}
---
+++ tests/Unit/NormalizeProductToFlatFormatTestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit;
+
+use App\Domain\Model\Product;
+use App\Domain\Normalizer\ProductNormalizer;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+
+class NormalizeProductToFlatFormatTestCase extends TestCase
+{
+    public function test_it_transform_properties_to_flat_format(): void
+    {
+        $product = new Product('my_product', ['shoes', 'clothes']);
+        $normalizer = new ProductNormalizer();
+
+        Assert::assertSame(
+            [
+                'identifier' => 'my_product',
+                'categories' => 'shoes,clothes'
+            ],
+            $normalizer->toFlatFormat($product)
+        );
+    }
+}
