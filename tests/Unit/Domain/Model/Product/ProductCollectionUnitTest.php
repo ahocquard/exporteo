22dd9f956a890de610a7213949f5380f47f30375
ahocquard <alexandre.hocquard@akeneo.com>
2019-06-02 23:19:49 +0200

    Tes normalizing of price values

--- tests/Unit/Domain/Model/Product/ProductCollectionUnitTest.php
+++ tests/Unit/Domain/Model/Product/ProductCollectionUnitTest.php
@@ -8,6 +8,7 @@ use App\Domain\Model\ExportHeaders;
 use App\Domain\Model\Product\Product;
 use App\Domain\Model\Product\ProductCollection;
 use App\Domain\Model\Product\Value\ArrayValue;
+use App\Domain\Model\Product\Value\PriceValue;
 use App\Domain\Model\Product\Value\ScalarValue;
 use App\Domain\Model\Product\ValueCollection;
 use PHPUnit\Framework\Assert;
@@ -27,6 +28,30 @@ class ProductCollectionUnitTest extends TestCase
                     new ArrayValue('attribute_code_6', 'en_US', null, ['foo', 'baz']),
                     new ArrayValue('attribute_code_7', null, 'tablet', ['foo', 'baz']),
                     new ArrayValue('attribute_code_8', 'fr_FR', 'ecommerce', ['foo', 'baz']),
+                    new PriceValue('attribute_code_9', null, null,
+                        [
+                            ['amount' => '45.00', 'currency' => 'USD'],
+                            ['amount' => '50.50', 'currency' => 'EUR']
+                        ]
+                    ),
+                    new PriceValue('attribute_code_10', 'fr_FR', null,
+                        [
+                            ['amount' => '45.00', 'currency' => 'USD'],
+                            ['amount' => '50.50', 'currency' => 'EUR']
+                        ]
+                    ),
+                    new PriceValue('attribute_code_11', null, 'ecommerce',
+                        [
+                            ['amount' => '45.00', 'currency' => 'USD'],
+                            ['amount' => '50.50', 'currency' => 'EUR']
+                        ]
+                    ),
+                    new PriceValue('attribute_code_12', 'fr_FR', 'ecommerce',
+                        [
+                            ['amount' => '45.00', 'currency' => 'USD'],
+                            ['amount' => '50.50', 'currency' => 'EUR']
+                        ]
+                    ),
                 ),
             ),
             new Product('my_product_2', [], new ValueCollection())
@@ -44,6 +69,14 @@ class ProductCollectionUnitTest extends TestCase
             'attribute_code_6-en_US',
             'attribute_code_7-tablet',
             'attribute_code_8-fr_FR-ecommerce',
+            'attribute_code_9-USD',
+            'attribute_code_9-EUR',
+            'attribute_code_10-fr_FR-USD',
+            'attribute_code_10-fr_FR-EUR',
+            'attribute_code_11-ecommerce-USD',
+            'attribute_code_11-ecommerce-EUR',
+            'attribute_code_12-fr_FR-ecommerce-USD',
+            'attribute_code_12-fr_FR-ecommerce-EUR',
             'attribute_code_extra',
             'categories',
             'identifier',
@@ -53,6 +86,12 @@ class ProductCollectionUnitTest extends TestCase
             [
                 [
                     'attribute_code_1' => 'data_1',
+                    'attribute_code_10-fr_FR-EUR' => '50.50',
+                    'attribute_code_10-fr_FR-USD' => '45.00',
+                    'attribute_code_11-ecommerce-EUR' => '50.50',
+                    'attribute_code_11-ecommerce-USD' => '45.00',
+                    'attribute_code_12-fr_FR-ecommerce-EUR' => '50.50',
+                    'attribute_code_12-fr_FR-ecommerce-USD' => '45.00',
                     'attribute_code_2-en_US' => 'data_2',
                     'attribute_code_3-tablet' => 'data_3',
                     'attribute_code_4-fr_FR-ecommerce' => 'data_4',
@@ -60,12 +99,20 @@ class ProductCollectionUnitTest extends TestCase
                     'attribute_code_6-en_US' => 'foo,baz',
                     'attribute_code_7-tablet' => 'foo,baz',
                     'attribute_code_8-fr_FR-ecommerce' => 'foo,baz',
+                    'attribute_code_9-EUR' => '50.50',
+                    'attribute_code_9-USD' => '45.00',
                     'attribute_code_extra' => null,
                     'categories' => 'shoes,clothes',
                     'identifier' => 'my_product_1',
                 ],
                 [
                     'attribute_code_1' => null,
+                    'attribute_code_10-fr_FR-EUR' => null,
+                    'attribute_code_10-fr_FR-USD' => null,
+                    'attribute_code_11-ecommerce-EUR' => null,
+                    'attribute_code_11-ecommerce-USD' => null,
+                    'attribute_code_12-fr_FR-ecommerce-EUR' => null,
+                    'attribute_code_12-fr_FR-ecommerce-USD' => null,
                     'attribute_code_2-en_US' => null,
                     'attribute_code_3-tablet' => null,
                     'attribute_code_4-fr_FR-ecommerce' => null,
@@ -73,6 +120,8 @@ class ProductCollectionUnitTest extends TestCase
                     'attribute_code_6-en_US' => null,
                     'attribute_code_7-tablet' => null,
                     'attribute_code_8-fr_FR-ecommerce' => null,
+                    'attribute_code_9-EUR' => null,
+                    'attribute_code_9-USD' => null,
                     'attribute_code_extra' => null,
                     'categories' => '',
                     'identifier' => 'my_product_2',
